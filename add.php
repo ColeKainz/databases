c14d61d2b451620ef8c6b14e6fd265c025a3d4ec
Th3MiteeyLambo <Th3MiteeyLambo@hotmail.com>
2015-12-04 20:40:30 -0600

    maybe

--- add.php
+++ add.php
@@ -12,7 +12,7 @@ function options($flag)
 
 
 		$conn = oci_connect('swam', 'sa7y7awv', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db1.chpc.ndsu.nodak.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
-		$query = "SELECT empid, fname, lname FROM  Employee";
+		$query = "SELECT * FROM  Employee";
 		$stid = oci_parse($conn,$query);
 		oci_execute($stid,OCI_DEFAULT);
 		$j = 0;
@@ -39,7 +39,7 @@ function options($flag)
 
 		    $employees[$i] = $row['fname'] . " " . $row['lname'];
 		    $employeeids[$i] = $row['empid'];
-		    $i = $i + 1;
+		    $j = $j + 1;
 		}
 		oci_free_statement($stid);
 		oci_close($conn);
