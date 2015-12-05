0c067fb103a6725d22365e3df6803a99a1dd4650
Th3MiteeyLambo <Th3MiteeyLambo@hotmail.com>
2015-12-05 15:38:32 -0600

    NIGGA$ WE FREEEEEEEEEE

--- add_data.php
+++ add_data.php
@@ -9,7 +9,7 @@
 
 	$conn = oci_connect('swam', 'sa7y7awv', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db1.chpc.ndsu.nodak.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
 	$query = "INSERT INTO Device" . " VALUES ('" . $_POST['devid'] . "', " . $_POST['Employees'] . ", '" . $_POST['Processors'] . "', '" . 
-		$_POST['Memories'] . "', '" . $_POST['Cards'] . "', " . $_POST['rack'] . ", " . $_POST['topUnit'] . ", '" . $_POST['typid'] . ");";
+		$_POST['Memories'] . "', '" . $_POST['Cards'] . "', " . $_POST['rack'] . ", " . $_POST['topUnit'] . ", '" . $_POST['typid'] . "');";
 	$stid = oci_parse($conn,$query);
 	oci_execute($stid,OCI_DEFAULT);
 	oci_free_statement($stid);	
