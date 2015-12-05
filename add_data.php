4e3f83a29831aa10cad565bb5cb1dcaf9e69c370
Th3MiteeyLambo <Th3MiteeyLambo@hotmail.com>
2015-12-05 16:41:25 -0600

    NIGGA THIS SHIT WORKS

--- add_data.php
+++ add_data.php
@@ -1,66 +1,6 @@
 <!DOCTYPE HTML>
 <html>
 
-	<?php
-
-		$conn = oci_connect('swam', 'sa7y7awv', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db1.chpc.ndsu.nodak.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
-		if(!$conn)
-		{
-			echo "An error has occurred!";
-		}
-		$query = "INSERT INTO Location VALUES(" . $_POST['building'] . ", " . $_POST['rack'] . ", " . $_POST['topUnit'] . ");";
-		echo $query;
-		$stid = oci_parse($conn,$query);
-		oci_execute($stid,OCI_COMMIT_ON_SUCCESS);
-		oci_free_statement($stid);	
-		oci_close($conn);
-
-		$conn = oci_connect('swam', 'sa7y7awv', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db1.chpc.ndsu.nodak.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
-		if(!$conn)
-		{
-			echo "An error has occurred!";
-		}
-		$query = "INSERT INTO Device" . " VALUES('" . $_POST['devid'] . "', " . $_POST['Employees'] . ", '" . $_POST['Processors'] . "', '" . $_POST['Memories'] . "', '" . $_POST['Cards'] . "', " . $_POST['rack'] . ", " . $_POST['topUnit'] . ", '" . $_POST['typid'] . "');";
-		echo $query;
-		$stid = oci_parse($conn,$query);
-		oci_execute($stid,OCI_COMMIT_ON_SUCCESS);
-		oci_free_statement($stid);	
-		oci_close($conn);
-
-
-		echo "You have just entered:"
-
-		$conn = oci_connect('swam', 'sa7y7awv', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db1.chpc.ndsu.nodak.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
-		$query = 'SELECT * FROM Device WHERE devid = ' . $_POST['devid'] . ');';
-		$stid = oci_parse($conn,$query);
-		oci_execute($stid,OCI_DEFAULT);
-		while ($row = oci_fetch_array($+stid,OCI_ASSOC)) 
-		{
-		   foreach ($row as $item) 
-		   {
-		      echo $item.' '; 
-		   }
-		   echo '<br/>';
-		}
-		oci_free_statement($stid);
-		oci_close($conn);
-
-		$conn = oci_connect('swam', 'sa7y7awv', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db1.chpc.ndsu.nodak.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
-		$query = 'SELECT * FROM Location WHERE rack = ' . $_POST['rack'] . ', topu = ' . $_POST['topUnit'] . ');';
-		$stid = oci_parse($conn,$query);
-		oci_execute($stid,OCI_DEFAULT);
-		while ($row = oci_fetch_array($+stid,OCI_ASSOC)) 
-		{
-		   foreach ($row as $item) 
-		   {
-		      echo $item.' '; 
-		   }
-		   echo '<br/>';
-		}
-		oci_free_statement($stid);
-		oci_close($conn);
-	?>
-
 	<head>
 
 		<script type="text/javascript" src="tabber.js"></script>
@@ -85,7 +25,65 @@
 			  <li><a href="add.html">Ad</a></li>
 			</ul> 
 		</div>
-		
+			<?php
+
+				$conn = oci_connect('swam', 'sa7y7awv', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db1.chpc.ndsu.nodak.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
+				if(!$conn)
+				{
+					echo "An error has occurred!";
+				}
+				$query = "INSERT INTO Location VALUES(" . $_POST['building'] . ", " . $_POST['rack'] . ", " . $_POST['topUnit'] . ");";
+				echo $query;
+				$stid = oci_parse($conn,$query);
+				oci_execute($stid,OCI_COMMIT_ON_SUCCESS);
+				oci_free_statement($stid);	
+				oci_close($conn);
+
+				$conn = oci_connect('swam', 'sa7y7awv', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db1.chpc.ndsu.nodak.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
+				if(!$conn)
+				{
+					echo "An error has occurred!";
+				}
+				$query = "INSERT INTO Device" . " VALUES('" . $_POST['devid'] . "', " . $_POST['Employees'] . ", '" . $_POST['Processors'] . "', '" . $_POST['Memories'] . "', '" . $_POST['Cards'] . "', " . $_POST['rack'] . ", " . $_POST['topUnit'] . ", '" . $_POST['typid'] . "');";
+				echo $query;
+				$stid = oci_parse($conn,$query);
+				oci_execute($stid,OCI_COMMIT_ON_SUCCESS);
+				oci_free_statement($stid);	
+				oci_close($conn);
+
+
+				echo "You have just entered:"
+
+				$conn = oci_connect('swam', 'sa7y7awv', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db1.chpc.ndsu.nodak.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
+				$query = 'SELECT * FROM Device WHERE devid = ' . $_POST['devid'] . ');';
+				$stid = oci_parse($conn,$query);
+				oci_execute($stid,OCI_DEFAULT);
+				while ($row = oci_fetch_array($+stid,OCI_ASSOC)) 
+				{
+				   foreach ($row as $item) 
+				   {
+				      echo $item.' '; 
+				   }
+				   echo '<br/>';
+				}
+				oci_free_statement($stid);
+				oci_close($conn);
+
+				$conn = oci_connect('swam', 'sa7y7awv', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db1.chpc.ndsu.nodak.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
+				$query = 'SELECT * FROM Location WHERE rack = ' . $_POST['rack'] . ', topu = ' . $_POST['topUnit'] . ');';
+				$stid = oci_parse($conn,$query);
+				oci_execute($stid,OCI_DEFAULT);
+				while ($row = oci_fetch_array($+stid,OCI_ASSOC)) 
+				{
+				   foreach ($row as $item) 
+				   {
+				      echo $item.' '; 
+				   }
+				   echo '<br/>';
+				}
+				oci_free_statement($stid);
+				oci_close($conn);
+			?>
 		</div>
 	</body>
 </html>
