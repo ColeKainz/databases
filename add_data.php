7afb8b3f720f6121467db696d1b3332ff813cbae
Th3MiteeyLambo <Th3MiteeyLambo@hotmail.com>
2015-12-05 15:55:18 -0600

    This one should work!

--- add_data.php
+++ add_data.php
@@ -3,9 +3,9 @@
 	$conn = oci_connect('swam', 'sa7y7awv', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db1.chpc.ndsu.nodak.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
 	if(!$conn)
 	{
-		echo "SHITS BROKE";
+		echo "An error has occurred!";
 	}
-	$query = "INSERT INTO Location VALUES (" . $_POST['building'] . ", " . $_POST['rack'] . ", " . $_POST['topUnit'] . ");";
+	$query = "INSERT INTO Location VALUES ( " . $_POST['building'] . ", " . $_POST['rack'] . ", " . $_POST['topUnit'] . ");";
 	$stid = oci_parse($conn,$query);
 	oci_execute($stid,OCI_DEFAULT);
 	oci_free_statement($stid);	
@@ -14,9 +14,9 @@
 	$conn = oci_connect('swam', 'sa7y7awv', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db1.chpc.ndsu.nodak.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
 	if(!$conn)
 	{
-		echo "SHIT'S BROKEN";
+		echo "An error has occurred!";
 	}
-	$query = "INSERT INTO Device" . " VALUES ('" . $_POST['devid'] . "', " . $_POST['Employees'] . ", '" . $_POST['Processors'] . "', '" . $_POST['Memories'] . "', '" . $_POST['Cards'] . "', " . $_POST['rack'] . ", " . $_POST['topUnit'] . ", '" . $_POST['typid'] . "');";
+	$query = "INSERT INTO Device" . " VALUES ( '" . $_POST['devid'] . "', " . $_POST['Employees'] . ", '" . $_POST['Processors'] . "', '" . $_POST['Memories'] . "', '" . $_POST['Cards'] . "', " . $_POST['rack'] . ", " . $_POST['topUnit'] . ", '" . $_POST['typid'] . "');";
 	$stid = oci_parse($conn,$query);
 	oci_execute($stid,OCI_DEFAULT);
 	oci_free_statement($stid);	
