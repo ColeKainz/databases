2c4038808085f83ebd0d2b5dbd38d1381b35322b
Th3MiteeyLambo <Th3MiteeyLambo@hotmail.com>
2015-12-05 16:06:05 -0600

    addddddd

--- add_data.php
+++ add_data.php
@@ -5,7 +5,8 @@
 	{
 		echo "An error has occurred!";
 	}
-	$query = "INSERT INTO Location VALUES ( " . $_POST['building'] . ", " . $_POST['rack'] . ", " . $_POST['topUnit'] . ");";
+	$query = "INSERT INTO Location VALUES(" . $_POST['building'] . ", " . $_POST['rack'] . ", " . $_POST['topUnit'] . ");";
+	echo $query;
 	$stid = oci_parse($conn,$query);
 	oci_execute($stid,OCI_DEFAULT);
 	oci_free_statement($stid);	
@@ -16,15 +17,45 @@
 	{
 		echo "An error has occurred!";
 	}
-	$query = "INSERT INTO Device" . " VALUES ( '" . $_POST['devid'] . "', " . $_POST['Employees'] . ", '" . $_POST['Processors'] . "', '" . $_POST['Memories'] . "', '" . $_POST['Cards'] . "', " . $_POST['rack'] . ", " . $_POST['topUnit'] . ", '" . $_POST['typid'] . "');";
+	$query = "INSERT INTO Device" . " VALUES('" . $_POST['devid'] . "', " . $_POST['Employees'] . ", '" . $_POST['Processors'] . "', '" . $_POST['Memories'] . "', '" . $_POST['Cards'] . "', " . $_POST['rack'] . ", " . $_POST['topUnit'] . ", '" . $_POST['typid'] . "');";
+	echo $query;
 	$stid = oci_parse($conn,$query);
 	oci_execute($stid,OCI_DEFAULT);
 	oci_free_statement($stid);	
 	oci_close($conn);
 
-	foreach($_POST as $row)
+	/*foreach($_POST as $row)
 	{
 		echo $row . "<br/>";
-	}
+	}*/
 
+	$conn = oci_connect('swam', 'sa7y7awv', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db1.chpc.ndsu.nodak.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
+	$query = 'SELECT * FROM Device WHERE devid = ' . $_POST['devid'] . ');';
+	$stid = oci_parse($conn,$query);
+	oci_execute($stid,OCI_DEFAULT);
+	while ($row = oci_fetch_array($+stid,OCI_ASSOC)) 
+	{
+	   foreach ($row as $item) 
+	   {
+	      echo $item.' '; 
+	   }
+	   echo '<br/>';
+	}
+	oci_free_statement($stid);
+	oci_close($conn);
+
+	$conn = oci_connect('swam', 'sa7y7awv', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db1.chpc.ndsu.nodak.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
+	$query = 'SELECT * FROM Location WHERE rack = ' . $_POST['rack'] . ', topu = ' . $_POST['topUnit'] . ');';
+	$stid = oci_parse($conn,$query);
+	oci_execute($stid,OCI_DEFAULT);
+	while ($row = oci_fetch_array($+stid,OCI_ASSOC)) 
+	{
+	   foreach ($row as $item) 
+	   {
+	      echo $item.' '; 
+	   }
+	   echo '<br/>';
+	}
+	oci_free_statement($stid);
+	oci_close($conn);
 ?>
